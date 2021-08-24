bcaf02c086435f5cb6839a116759ef68ba0b5412
Léo <leo.catifait@epsi.fr>
2021-08-24 14:55:27 +0200

    [TRY] modify css for iphone 7+

--- Vue/index.php
+++ Vue/index.php
@@ -6,13 +6,15 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="description" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="keywords" content="Léo Catifait"/>
+    <meta http-equiv="cache-control" content="no-cache"/>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/dfde2568e7.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css" type="text/css"/>
-    <title></title>
+    <title>Portefolio Léo Catifait</title>
 </head>
 <body>
 <h1 >Kraystone</h1>
